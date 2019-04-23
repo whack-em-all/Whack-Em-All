5ddd0ef82d30623567769bc43b1a4f8fde653be4
eltim12 <eltim2000@gmail.com>
2019-04-23 13:37:03 +0700

    added sync image

--- client/src/api/firebaseAPI.js
+++ client/src/api/firebaseAPI.js
@@ -1,14 +1,22 @@
 import firebase from 'firebase';
 
-const config = {
-  apiKey: 'AIzaSyBFbJiRGH1KO9wUXACk1Ue1vH-gtbfll4I',
-  authDomain: 'whack-em-all.firebaseapp.com',
-  databaseURL: 'https://whack-em-all.firebaseio.com',
-  projectId: 'whack-em-all',
-  storageBucket: 'whack-em-all.appspot.com',
-  messagingSenderId: '844310209802',
-};
+// const config = {
+//   apiKey: 'AIzaSyBFbJiRGH1KO9wUXACk1Ue1vH-gtbfll4I',
+//   authDomain: 'whack-em-all.firebaseapp.com',
+//   databaseURL: 'https://whack-em-all.firebaseio.com',
+//   projectId: 'whack-em-all',
+//   storageBucket: 'whack-em-all.appspot.com',
+//   messagingSenderId: '844310209802',
+// };
 
+var config = {
+  apiKey: "AIzaSyBFbJiRGH1KO9wUXACk1Ue1vH-gtbfll4I",
+  authDomain: "whack-em-all.firebaseapp.com",
+  databaseURL: "https://whack-em-all.firebaseio.com",
+  projectId: "whack-em-all",
+  storageBucket: "whack-em-all.appspot.com",
+  messagingSenderId: "844310209802"
+};
 firebase.initializeApp(config);
 
 const db = firebase.firestore();
